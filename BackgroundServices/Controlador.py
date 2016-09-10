0533fc3a9b4ba2c24ab42617c397d8be14ad4453
Gustavo Frainer <gustavo.frainer@hotmail.com>
2016-09-10 18:46:17 -0300

    BackgroundServices Controlador

---
+++ BackgroundServices/Controlador.py
@@ -0,0 +1,15 @@
+class Controlador:
+    Timer = None
+    Database = None
+    Configuracao = None
+    Log = None
+
+    def verificar(self):
+        return
+    def carregar(self):
+        return
+    def carregar_coleta(self):
+        return
+    def carregar_refinamento(self):
+        return
+
