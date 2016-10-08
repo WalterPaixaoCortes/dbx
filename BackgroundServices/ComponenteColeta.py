502b5aa41bf96f76e800428f98d82b96ef37e2a9
carolinaNery94 <carolina.nery94@gmail.com>
2016-10-08 18:17:42 -0300

    Componente de coleta, estrutura (commit parcial)

--- BackgroundServices/ComponenteColeta.py
+++ BackgroundServices/ComponenteColeta.py
@@ -5,3 +5,15 @@ class ComponenteColeta(Componente):
     def __init__(self, d, t):
         Componente.__init__(self, d)
         return
+
+    #busca e coleta de informações das bases de dados externas
+    def extract(self):
+        return
+
+    #normalização dos dados
+    def parse(self):
+        return
+
+    #salvar o resultado no banco de dados interno
+    def save(self):
+        return
