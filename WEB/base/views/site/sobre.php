621980b93d3dae4a3435b45566e9e98cd299a092
Gustavo Frainer <gustavo.frainer@hotmail.com>
2016-11-05 18:52:52 -0200

    Sobre

--- WEB/base/views/site/sobre.php
+++ WEB/base/views/site/sobre.php
@@ -11,13 +11,13 @@ $this->title = 'DBX Sobre';
     </div>
 
     <div class="body-content" style="font-size: small !important;">
-        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">DB-X é uma evolução do sistema de banco de dados DB-InhA, sistema voltado exclusivamente a informações sobre a enzima InhA da Mycobacterium tuberculosis.</p>
-        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">DB-X é uma versão do sistema DB-InhA que foi modificada para torná-lo mais abrangente e prático, fazendo com que a sua estrutura seja modular e, consequentemente, possibilitando uma maior escalabilidade e permitindo que componentes sejam desenvolvidos separadamente.</p>
-        <p style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important; margin: 0px !important;">Dentre as principais funcionalidades estão:</p>
+        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">DB-X é uma evolução do sistema DB-InhA, sistema voltado exclusivamente a informações sobre a enzima InhA da <i>Mycobacterium tuberculosis.</i></p>
+        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">Pensando em tornar o sistema DB-InhA mais abrangente e prático, nasceu o DB-X.
+            O DB-X foi criado a fim de apresentar um sistema para a centralização das buscas de informações sobre proteínas, que seja funcional com quaisquer proteínas e permita o cadastro das mesmas sem a necessidade de alteração do código por um profissional de TI. </p>
+        <p style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important; margin: 0px !important;">Com isso você pode:</p>
         <ul style="margin-left: 60px;">
             <li>Buscar informação</li>
-            <li>Baixar informações relacionadas as enzimas</li>
-            <li>Visualizar informação</li>
+            <li>Visualizar informações relacionadas às enzimas e seus ligantes</li>
         </ul>
 
     </div>
