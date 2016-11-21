3adaa46d87debcf4a7a253d45f86d80c7287d557
gustavo <bakaneko*5>
2016-11-21 00:53:55 -0200

    Ajustes

--- WEB/base/views/site/index.php
+++ WEB/base/views/site/index.php
@@ -3,13 +3,10 @@
 $this->title = 'DBX';
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h2>Bem-vindo ao DB-X!</h2></br>
-
-        <p>O DB-X é uma forma centralizada de realizar a visualização de dados e buscas de artigos e registros de enzimas de plataformas de publicações sobre bioinformática.</p>
-    </div>
-
     <div class="body-content">
+        <h2 style="text-align: center">Bem-vindo ao DB-X!</h2></br>
+
+        <p style="text-align: justify; text-indent: 3em">A fim de tratar as dificuldades inerentes ao processo de pesquisa de proteínas, foi desenvolvido o sistema DB-X. Este sistema constitui-se de uma interface web responsiva, que permite a realização de buscas de artigos e estruturas de proteínas de forma centralizada, redirecionando o usuário para o sistema de origem quando a informação relevante for encontrada.</p>
+        <p style="text-align: justify; text-indent: 3em">O DB-X  foi baseado no conceito do sistema DB-InhA, o qual permite a busca de informações apenas da proteína InhA de Mycobacterium tuberculosis provenientes de várias fontes distintas. DB-X foi desenvolvido para funcionar de forma modular e genérica, permitindo a busca por quaisquer proteínas de interesse e maior flexibilidade em suas alterações e incrementos, oferecendo, assim, maior escalabilidade ao seu administrador.</p>
     </div>
 </div>
--- WEB/base/views/site/sobre.php
+++ WEB/base/views/site/sobre.php
@@ -3,19 +3,19 @@
 $this->title = 'DBX Sobre';
 ?>
 <div class="">
-
-    <div class="jumbotron">
-            <h3>Sobre</h3>
-    </div>
-
-    <div class="body-content" style="font-size: small !important;">
-        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">DB-X é uma evolução do sistema DB-InhA, sistema voltado exclusivamente a informações sobre a enzima InhA da <i>Mycobacterium tuberculosis.</i></p>
-        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">Pensando em tornar o sistema DB-InhA mais abrangente e prático, nasceu o DB-X.
-            O DB-X foi criado a fim de apresentar um sistema para a centralização das buscas de informações sobre proteínas, que seja funcional com quaisquer proteínas e permita o cadastro das mesmas sem a necessidade de alteração do código por um profissional de TI. </p>
-        <p style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important; margin: 0px !important;">Com isso você pode:</p>
-        <ul style="margin-left: 60px;">
-            <li>Buscar informação</li>
-            <li>Visualizar informações relacionadas às enzimas e seus ligantes</li>
+    <div class="body-content" style="font-size: small !important; margin-top: 30px">
+        <h4 style="text-align: center">Introdução</h4>
+        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">A Bioinformática gera e, também, analisa grandes volumes de dados provenientes de estudos de genômica, proteômica e transcriptômica que servem como base para novas pesquisas. No entanto, esses dados estão dispersos em diversos sistemas de informação, como diferentes bancos de dados, e não possuem padronização.</p>
+        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">Devido a essa falta de padronização e a dispersão das informações disponíveis atualmente, principalmente sobre proteínas de interesse biomédico, o acesso a essas informações torna-se uma atividade complexa e ineficiente, constituindo um empecilho no desenvolvimento de novos estudos. Dessa forma, faz-se necessário o desenvolvimento de um sistema centralizador de buscas de informações sobre proteínas.</p>
+        </br>
+        <h4 style="text-align: center">Sobre o DB-X</h4>
+        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">A fim de tratar as dificuldades encontradas, foi desenvolvido o sistema DB-X. Este sistema constitui-se de uma interface web responsiva, que permite a realização de buscas de artigos e estruturas de proteínas de forma centralizada, redirecionando o usuário para o sistema de origem quando a informação relevante for encontrada.</p>
+        <p class="lead" style="text-indent: 30px; padding: 0 50px 0  50px; text-align: justify; font-size: medium !important;">
+            O DB-X  foi baseado no conceito do sistema DB-InhA [1], o qual permite a busca de informações apenas da proteína InhA de Mycobacterium tuberculosis provenientes de várias fontes distintas. DB-X foi desenvolvido para funcionar de forma modular e genérica, permitindo a busca por quaisquer proteínas de interesse e maior flexibilidade em suas alterações e incrementos, oferecendo, assim, maior escalabilidade ao seu administrador.</p>
+        <p style="text-indent: 30px; padding: 0 50px 0  50px; text-align: center; font-size: medium !important; margin: 0px !important;">Equipe de Desenvolvimento:</p>
+        <ul style="text-align: center">
+            <li>Carolina Oltramari Nery, e-mail: carolina.nery@acad.pucrs.br</li>
+            <li>Gustavo Savi Frainer, e-mail: gustavo.frainer@acad.pucrs.br</li>
         </ul>
 
     </div>
